e143b27251a3562be76395f723c6f0f492336f80
SLIPPECAT <119715555+SLIPPECAT@users.noreply.github.com>
2023-03-01 22:45:02 +0900

    Add files via upload

---
+++ report_api3/src/main/java/com/example/report_api3/ReportApi3Application.java
@@ -0,0 +1,13 @@
+package com.example.report_api3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportApi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportApi3Application.class, args);
+    }
+
+}
---
+++ report_api3/src/test/java/com/example/report_api3/ReportApi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.report_api3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportApi3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
